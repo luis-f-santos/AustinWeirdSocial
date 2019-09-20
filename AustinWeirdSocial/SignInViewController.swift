82dee0589b4cfd95e496bbda979d043726ea1f5b
Luis Santos <lfsantos2008@Luiss-MacBook-Pro.local>
2019-09-19 21:37:49 -0500

    Finishing social setup

---
+++ AustinWeirdSocial/SignInViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AustinWeirdSocial
+//
+//  Created by Luis Santos on 9/18/19.
+//  Copyright © 2019 Luis Santos. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var testing = "test"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
