8063e6eaf9fd858451a1b440952fd5d6b13c9279
Luis Santos <lfsantos2008@Luiss-MacBook-Pro.local>
2019-09-24 23:13:10 -0500

    adding feed UI

--- AustinWeirdSocial/View/CircleView.swift
+++ AustinWeirdSocial/View/CircleView.swift
@@ -1,25 +1,27 @@
 //
-//  ShadowView.swift
+//  CircleView.swift
 //  AustinWeirdSocial
 //
-//  Created by Luis Santos on 9/19/19.
+//  Created by Luis Santos on 9/24/19.
 //  Copyright © 2019 Luis Santos. All rights reserved.
 //
 
 import UIKit
 
-class ShadowView: UIView {
+class CircleView: UIImageView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 9.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-        
-        
+    }
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+
     }
 
 }
