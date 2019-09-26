b2351d03f2192c399eff7aa35799d51e268e5c67
Luis Santos <lfsantos2008@Luiss-MacBook-Pro.local>
2019-09-26 02:53:38 -0500

    creating Post model and connecting captiona and num likes to UI

--- AustinWeirdSocial/View/PostCell.swift
+++ AustinWeirdSocial/View/PostCell.swift
@@ -16,21 +16,19 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likesLbl: UILabel!
 
-    
+    var post: Post!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     
-    
-    
-    
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func configureCell(post: Post) {
+        
+        self.post = post
+        self.caption.text = post.caption
+        self.likesLbl.text = "\(post.likes)"
+        
     }
 
 }
